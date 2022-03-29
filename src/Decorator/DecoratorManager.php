f83b9b38ad23103ed1df0b18be689ecbdfe3b394
Heavlock <33984264+Heavlock@users.noreply.github.com>
2022-03-29 10:46:06 +0500

    Initial commit

---
+++ src/Decorator/DecoratorManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace src\Decorator;
+
+use DateTime;
+use Exception;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+use src\Integration\DataProvider;
+
+class DecoratorManager extends DataProvider
+{
+    public $cache;
+    public $logger;
+
+
+    /**
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param CacheItemPoolInterface $cache
+     */
+    public function __construct($host, $user, $password, CacheItemPoolInterface $cache)
+    {
+        parent::__construct($host, $user, $password);
+        //TODO комментарии по поводу данных доступа $host, $user, $password в классе DataProvider
+
+        $this->cache = $cache;
+        //TODO кеширование и логирование наверняка используются не только в данном объекте
+        // и передавать их как аргументы не очень удобно, было бы лучше сделать как-то так
+        // use Support/Cache
+        // use Support/Logger
+        //  $this->cache = new Cache;
+        //  $this->logger = new Logger;
+        // либо работать через статичные методы соответствующих классов
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        //TODO $logger лучше перенести в __construct, чтобы не вызывать метод вручную
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    //TODO непонятный метод {@inheritdoc} не нашел его в коде
+    public function getResponse(array $input)
+    {
+        try {
+            $cacheKey = $this->getCacheKey($input);
+            $cacheItem = $this->cache->getItem($cacheKey);
+            if ($cacheItem->isHit()) {
+                return $cacheItem->get();
+            }
+
+            $result = parent::get($input);
+
+            $cacheItem
+                ->set($result)
+                ->expiresAt(
+                    (new DateTime())->modify('+1 day')
+                );
+
+            return $result;
+        } catch (Exception $e) {
+            $this->logger->critical('Error');
+            //TODO здесь бы я заменил вывод ссобщения 'error' на более понятное сообщение
+            // $this->logger->critical($e->getMessage());
+            // или $this->logger->critical('ошибка подключения к external service');
+        }
+
+        return [];
+    }
+
+    public function getCacheKey(array $input)
+    {
+        return json_encode($input);
+    }
+}
---
+++ src/Integration/DataProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace src\Integration;
+
+class DataProvider
+{
+    private $host;
+    private $user;
+    private $password;
+
+    /**
+     * @param $host
+     * @param $user
+     * @param $password
+     */
+    public function __construct($host, $user, $password)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        //TODO данные с доступами я бы хранил в отдельном конфиге, к примеру configs/externalService.php
+        // в нем [ host=>localhost, user=> mainUser, password=>123 ] и получал бы доступ к ним через методы - хелперы
+        // к примеру use Support\Config Config::get('externalService.password'), таким образом мы бы избавились
+        // от объявления лишних свойств в объекте DataProvider и переброску аргументов через дочерний класс DecoratorManager
+    }
+
+    /**
+     * @param array $request
+     *
+     * @return array
+     */
+    public function get(array $request)
+    {
+        // returns a response from external service
+    }
+}
